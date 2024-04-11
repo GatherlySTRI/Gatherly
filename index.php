fd4283558c305cc09d2a27521ae6ec37837c3039
Thomas Collet <t.collet974@gmail.com>
2024-04-11 19:57:51 +0000

    Initializing dependecies

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 if (empty($_GET['slug'])) //S'il n'y pas de slug.
 {
-    require_once("controllers/home/default.php");
+    require_once("src/controllers/home/default.php");
     
 } elseif (strpos($_GET['slug'], "/") !== false) { //Si le slug contient un slash.
     $slug = explode("/", $_GET['slug']);
@@ -12,13 +12,13 @@ if (empty($_GET['slug'])) //S'il n'y pas de slug.
 
     if (empty($slug1))
     {
-        require_once("controllers/$slug0/default.php");
+        require_once("src/controllers/$slug0/default.php");
     } else {
-        require_once("controllers/$slug0/$slug1.php");
+        require_once("src/controllers/$slug0/$slug1.php");
     }
 
 } else { //S'il n'y a pas de slash.
     $slug = trim($_GET['slug']);
 
-    require_once("controllers/$slug/default.php");
+    require_once("src/controllers/$slug/default.php");
 }
