1ade8843a6f72af9d39f5e772de2cdea39e3e1e9
Houston <23219631+HoustonLFB@users.noreply.github.com>
2024-04-11 10:18:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,7 @@
 
 if (empty($_GET['slug'])) //S'il n'y pas de slug.
 {
-    //TODO: acceder au controlleur de la page d'accueil
-
     require_once("controllers/home.php");
-    echo "1";
     
 } elseif (strpos($_GET['slug'], "/") !== false) { //Si le slug contient un slash.
     $slug = explode("/", $_GET['slug']);
@@ -14,14 +11,12 @@ if (empty($_GET['slug'])) //S'il n'y pas de slug.
     $slug1 = $slug[1];
 
     //TODO: acceder au controlleur
-    echo "2";
 
 } else { //S'il n'y a pas de slash.
     $slug = $_GET['slug'];
 
     //TODO: acceder au controlleur
 
-    require_once("controllers/$slug.php");
-
-    echo "3";
+    $controller = "controllers/$slug.php";
+    require_once($controller);
 }
