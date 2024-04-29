8566d7a0037ff8132396d11ebd4f5d895fbe3f37
Thomas97460 <t.collet974@outlook.fr>
2024-04-29 16:20:22 +0000

    change index php

---
+++ src/controllers/login.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use models\humain\Personne;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+$loader = new FilesystemLoader('src/view');
+$twig = new Environment($loader);
+
