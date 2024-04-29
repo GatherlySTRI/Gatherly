392e1fc33511c9af36e91b40fc31d6bd12ed565a
Thomas97460 <t.collet974@outlook.fr>
2024-04-29 20:20:08 +0000

    register

--- src/controllers/register.php
+++ src/controllers/register.php
@@ -1,7 +1,7 @@
 <?php
 namespace controllers\register;
 
-
+require_once 'src/controllers/tools/form_tools.php';
 require_once 'vendor/autoload.php';
 
 use models\humain\Personne;
@@ -11,19 +11,29 @@ use Twig\Environment;
 use controllers\tools\form_tools;
 
 function verify_all_params($params) {// Vérification de la présence et validité des paramètres
-    $required_params = ['name', 'prenom', 'date_naissance', 'sexe', 'email', 'telephone', 'password'];
+    $required_params = ['nom', 'prenom', 'date_naissance', 'sexe', 'email', 'telephone', 'mdp'];
     foreach ($required_params as $param) {
-        if ($params[$param] == "") {
+        if (!array_key_exists($param, $params) || $params[$param] == "") {
             return false;
         }
     }
-    if (!form_tools\is_date_valid($params['date_naissance'])) {
+    if (!form_tools\is_date_valid($params['date_naissance'])) {// Vérification de la validité de la date
+        return false;
+    }
+    elseif(!filter_var($params['email'], FILTER_VALIDATE_EMAIL)){// Vérification de la validité de l'email
+        return false;
+    }
+    elseif(!preg_match('/^[0-9]{10}$/', $params['telephone'])){// Vérification de la validité du numéro de téléphone
+        return false;
+    }
+    elseif(strlen($params['mdp'])<8){// Vérification de la validité du mot de passe
+        return false;
+    }
+    elseif($params['sexe'] != 'homme' && $params['sexe'] != 'femme'){// Vérification de la validité du sexe
         return false;
     }
     return true;
 }
-
-//Chargement de la vue
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {// Si la requête est de type POST
@@ -39,8 +49,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {// Si la requête est de type POST
     header('Location: login');
     exit;
 }
-
-$loader = new FilesystemLoader('src/view');
-$twig = new Environment($loader);
-
-echo $twig->render("register.twig");
+elseif($_SERVER['REQUEST_METHOD'] === 'GET'){// Si la requête est de type GET
+    //Chargement de la vue
+    $loader = new FilesystemLoader('src/view');
+    $twig = new Environment($loader);
+    echo $twig->render("register.twig");
+}
+else{
+    http_response_code(405);
+}
--- src/controllers/tools/form_tools.php
+++ src/controllers/tools/form_tools.php
@@ -1,11 +1,8 @@
 <?php
-namespace form_tools;
-
-require_once 'vendor/autoload.php';
+namespace controllers\tools\form_tools;
 
 function is_date_valid($date, $format = 'Y-m-d'){
-    $d = DateTime::createFromFormat($format, $date);
+    $d = \DateTime::createFromFormat($format, $date);
     return $d && $d->format($format) === $date;
 }
-
 ?>
