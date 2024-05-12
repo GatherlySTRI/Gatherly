afc6247703e790bf04d5a234a7aba30bf84675f2
Houston <23219631+HoustonLFB@users.noreply.github.com>
2024-05-12 12:03:34 +0200

    chore: Remove unnecessary session_start() call and fix code formatting in show.php

--- src/controllers/event/show.php
+++ src/controllers/event/show.php
@@ -8,7 +8,6 @@ use models\humain\Utilisateur;
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-session_start();
 // Vérifier si l'utilisateur est connecté
 if (!isset($_SESSION['id_utilisateur'])) {
     // L'utilisateur est connecté, rediriger vers la page d'accueil
@@ -16,7 +15,7 @@ if (!isset($_SESSION['id_utilisateur'])) {
     exit;
 }
 
-if($_SERVER['REQUEST_METHOD'] != 'GET'){// Vérification de la méthode de la requête
+if ($_SERVER['REQUEST_METHOD'] != 'GET') { // Vérification de la méthode de la requête
     http_response_code(405);
     exit;
 }
@@ -31,12 +30,12 @@ $organiser->find_by_column(null, 'id_evenement_organise', $id_evenement);
 $utilisateur = new Utilisateur();
 $utilisateur->find(null, $id_utilisateur);
 
-if($evenement->get_id_evenement() == null){// Vérification que l'évenement existe
+if ($evenement->get_id_evenement() == null) { // Vérification que l'évenement existe
     http_response_code(404);
     exit;
 }
 
-if($organiser->get_id_organisateur() != $_SESSION['id_utilisateur'] && !$utilisateur->get_est_admin()){// Vérification que l'évènement appartient bien à l'utilisateur ou si l'utilisateur est admin
+if ($organiser->get_id_organisateur() != $_SESSION['id_utilisateur'] && !$utilisateur->get_est_admin()) { // Vérification que l'évènement appartient bien à l'utilisateur ou si l'utilisateur est admin
     http_response_code(403);
     exit;
 }
@@ -45,6 +44,3 @@ $loader = new FilesystemLoader('src/view');
 $twig = new Environment($loader);
 
 echo $twig->render("event/show.twig", ['evenement' => $evenement->getData(), 'organiser' => $organiser, 'is_session' => isset($_SESSION['id_utilisateur'])]);
-
-
-?>
