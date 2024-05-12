cd0e7d76e9c07cd7607554e0dfbd1567e812a0b3
Houston <23219631+HoustonLFB@users.noreply.github.com>
2024-05-12 12:02:06 +0200

    chore: Update login controller to handle user authentication and session management

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -2,7 +2,7 @@
 require_once 'vendor/autoload.php';
 
 // Vérifier si l'utilisateur est connecté
-if (isset($_SESSION['id_utilisateur'])) {// Si l'utilisateur est connecté
+if (isset($_SESSION['id_utilisateur'])) { // Si l'utilisateur est connecté
     // Redirection vers la page d'accueil
     ob_start(); // Start output buffering
     header('Location: /');
@@ -11,6 +11,7 @@ if (isset($_SESSION['id_utilisateur'])) {// Si l'utilisateur est connecté
 }
 
 use models\humain\Utilisateur;
+use models\humain\Personne;
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
@@ -20,24 +21,22 @@ $twig = new Environment($loader);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $utilisateur = new Utilisateur();
     $utilisateur->find_by_column(null, 'mail', $_POST['mail']);
-    if ($utilisateur->get_id_utilisateur() == null || $utilisateur->get_mdp() != md5($_POST['mdp'])){
+    if ($utilisateur->get_id_utilisateur() == null || $utilisateur->get_mdp() != md5($_POST['mdp'])) {
         echo $twig->render("login.twig", ['error' => 'Email ou mot de passe incorrect', 'is_session' => isset($_SESSION['id_utilisateur'])]);
         exit;
-    }
-    elseif ($utilisateur->get_id_utilisateur() != null && $utilisateur->get_mdp() == md5($_POST['mdp'])){
+    } elseif ($utilisateur->get_id_utilisateur() != null && $utilisateur->get_mdp() == md5($_POST['mdp'])) {
         session_destroy();
         session_start();
         $_SESSION['id_utilisateur'] = $utilisateur->get_id_utilisateur();
+        $personne = new Personne();
+        $personne->find(null, $utilisateur->get_id_personne_utilisateur());
+        $_SESSION['prenom'] = $personne->get_prenom_personne();
         header('Location: /');
         exit;
     }
-}
-elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
     echo $twig->render("login.twig", ['is_session' => isset($_SESSION['id_utilisateur'])]);
-}
-else {
+} else {
     http_response_code(405);
     exit;
 }
-
-?>
